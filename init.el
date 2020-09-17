e81a720ce46907adde8b9f4932c87896f0c4ac41
Calvin Glisson <cglis001@ucr.edu>
2020-09-16 23:46:22 -0700

    Exwm

--- init.el
+++ init.el
@@ -189,6 +189,67 @@
 (setq lsp-idle-delay 0.5)
 
 ;; Other packages
+(use-package exwm
+  :config
+  (fringe-mode 1)
+  (setq display-time-default-load-average nil)
+  (display-time-mode t)
+  (server-start)
+  (setq exwm-workspace-number 4)
+
+  (add-hook 'exwm-update-class-hook
+            (lambda ()
+              (exwm-workspace-rename-buffer exwm-class-name)))
+  (setq exwm-input-global-keys
+      `(
+        ;; Bind "s-r" to exit char-mode and fullscreen mode.
+        ([?\s-r] . exwm-reset)
+        ;; Bind "s-w" to switch workspace interactively.
+        ([?\s-w] . exwm-workspace-switch)
+        ;; Bind "s-0" to "s-9" to switch to a workspace by its index.
+        ,@(mapcar (lambda (i)
+                    `(,(kbd (format "s-%d" i)) .
+                      (lambda ()
+                        (interactive)
+                        (exwm-workspace-switch-create ,i))))
+                  (number-sequence 0 9))
+        ;; Bind "s-p" to launch applications ('M-&' also works if the output
+        ;; buffer does not bother you).
+        ([?\s-p] . (lambda (command)
+		     (interactive (list (read-shell-command "$ ")))
+		     (start-process-shell-command command nil command)))
+        ;; Bind "s-<f2>" to "slock", a simple X display locker.
+        ([s-f2] . (lambda ()
+		    (interactive)
+		    (start-process "" nil "/usr/bin/slock")))))
+
+  (setq exwm-input-simulation-keys
+      '(
+        ;; movement
+        ([?\C-b] . [left])
+        ([?\M-b] . [C-left])
+        ([?\C-f] . [right])
+        ([?\M-f] . [C-right])
+        ([?\C-p] . [up])
+        ([?\C-n] . [down])
+        ([?\C-a] . [home])
+        ([?\C-e] . [end])
+        ([?\M-v] . [prior])
+        ([?\C-v] . [next])
+        ([?\C-d] . [delete])
+        ([?\C-k] . [S-end delete])
+        ;; cut/paste.
+        ([?\C-w] . [?\C-x])
+        ([?\M-w] . [?\C-c])
+        ([?\C-y] . [?\C-v])
+        ;; search
+        ([?\C-s] . [?\C-f])))
+
+  (exwm-enable)
+
+;  (require 'exwm-config)
+;  (exwm-config-default)
+  )
 (use-package multiple-cursors
   :bind ("C-c m" . mc/edit-lines))
 
